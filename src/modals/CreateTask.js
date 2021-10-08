b397c2fd49c1b59de01e4160575ed82737bd46f0
karthik <karthikm871994@gmail.com>
2021-10-08 15:55:22 +0530

    Modal changes

--- src/modals/CreateTask.js
+++ src/modals/CreateTask.js
@@ -5,10 +5,21 @@ const CreateTask = ({ modal, toggle }) => {
   return (
     <Modal isOpen={modal} toggle={toggle}>
       <ModalHeader toggle={toggle}>User Name</ModalHeader>
-      <ModalBody>About the user</ModalBody>
+      <ModalBody>
+        <form>
+          <div className="form-group">
+            <label>Task</label>
+            <input type="text" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label> Description </label>
+            <textarea rows="4" className="form-control"></textarea>
+          </div>
+        </form>
+      </ModalBody>
       <ModalFooter>
         <Button color="primary" onClick={toggle}>
-          Do Something
+          Create
         </Button>{" "}
         <Button color="secondary" onClick={toggle}>
           Cancel
